9c8c689b801649d04c3cbe0d7a0cbb0e330b885d
vmatveev <vmatveevspam@gmail.com>
2020-12-06 15:54:43 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+/*********************************************************************************
+* WEB422 – Assignment 06
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
+* assignment has been copied manually or electronically from any other source (including web sites) or
+* distributed to other students.
+*
+* Name: Vladislav Matveev Student ID: 032112138 Date: December 6, 2020
+*
+* Online Link: _______________________________________________________________
+*
+********************************************************************************/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'assignment4';
+}
